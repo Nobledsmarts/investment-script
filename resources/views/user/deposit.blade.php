2e76e00ef2e37210e519e224bddfb08b6f61db29
nobledsmarts <35113848+Nobledsmarts@users.noreply.github.com>
2022-04-30 22:44:58 +0100

    deposit page

--- resources/views/user/deposit.blade.php
+++ resources/views/user/deposit.blade.php
@@ -2,39 +2,61 @@
         <div id="main-wrapper">
             @include('user.layouts.navigation')
             @include('user.layouts.sidebar')
-            <div class="page-wrapper" data-wallet="{{ $wallets->count() }}">
-                <div class="row page-titles">
-                    <div class="col-md-5 align-self-center">
-                        <h3 class="text-light">Deposit</h3>
+            <div class="content-wrapper">
+                <div class="container-full">
+                    <div class="content-header">
+                        <div class="d-flex align-items-center">
+                            <div class="me-auto">
+                                <h4 class="page-title">Deposit</h4>
+                                <div class="d-inline-block align-items-center">
+                                    <nav>
+                                        <ol class="breadcrumb">
+                                            <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-home-outline"></i></a></li>
+                                            <li class="breadcrumb-item" aria-current="page">User</li>
+                                            <li class="breadcrumb-item active" aria-current="page">Deposit</li>
+                                        </ol>
+                                    </nav>
+                                </div>
+                            </div>
+                            
+                        </div>
                     </div>
-                    <div class="col-md-7 align-self-center">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="javascript:void(0)">User</a></li>
-                            <li class="breadcrumb-item active">Deposit</li>
-                        </ol>
-                    </div>
-                </div>
-
-                <div class="container-fluid">
-                    <div class="row">
-                        @foreach ($plans as $plan)
+                    <!-- Main content -->
+                    <section class="content">			
+                        
+                        
+                        <div class="row">
+                            @foreach ($plans as $plan)
                         <div class="col-lg-4 col-md-6">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h4 class="card-title">{{ $plan['name'] }} Plan</h4>
-                                    <div style="text-align: center"> <i class="fa fa-globe text-warning"></i>
-                                        <h5 style="font-size: 35px; font-weight: 600; color:rgb(255, 255, 255)" class="mt-3 mb-3">$ {{ $plan['minimum_amount'] }}</h5>
+                            <div class="box box-hover-shadow no-shadow">
+                                <div class="box-header with-border" style="background : #1f47a9">
+                                    <h4 class="box-title text-capitalize"><strong>{{ $plan['name'] }} Plan</strong> </h4>
+                                </div>
+                                <div class="box-body">
+                                    <!-- <h4 class="box-title">{{ $plan['name'] }} Plan</h4> -->
+                                    <div style="text-align: center">
+                                    <!-- <i data-feather="at-sign"></i> -->
+                                        <h5 style="font-size: 35px; color:rgb(255, 255, 255)" class="mt-3 mb-3">${{ number_format($plan['minimum_amount'], 2) }}</h5>
+                                        <p class="h5 plan-details"> Minimum Amount </p>
                                         <div class="divider"></div>
+                                        <h5 style="font-size: 35px; color:rgb(255, 255, 255)" class="mt-3 mb-3">${{ number_format($plan['maximum_amount'], 2) }}</h5>
+                                        <p class="h5 plan-details"> Maximum Amount </p>
+                                        <div class="divider"></div>
+                
+                                        <h5 style="font-size: 25px; color:rgb(255, 255, 255)" class="mt-3 mb-3">{{ $plan['interest_rate'] }}%</h5>
+                                        <p class="h5 plan-details"> Returns Daily For {{ $plan['duration'] }} days </p>
+                                        <div class="divider"></div>
+
+                                        <h5 style="font-size: 25px; color:rgb(255, 255, 255)" class="mt-3 mb-3">{{ $plan['referral_bonus'] }}%</h5>
+                                        <p class="h5 plan-details"> Referral Bonus </p>
+                                        <div class="divider"></div>
+
                                         <div class="">
-                                            <p class="h5 plan-details"> Min Amount: ${{ $plan['minimum_amount'] }} </p>
-                                            <p class="h5 plan-details"> Max Amount: ${{ $plan['maximum_amount'] }} </p>
-                                            <p class="h5 plan-details"> Upto ${{ $plan['interest_rate'] }} daily for {{ $plan['duration'] }}days </p>
-                                            <p class="h5 plan-details"> Principal Return </p>
-                                            <p class="h5 plan-details"> Compound Available </p>
+                                           
                                             <div class="plan-btn-cont">
                                                 <a href="javascript:void(0)" data-return="{{ $plan['interest_rate'] }}" 
                                                 data-child_plan_id="{{ $plan['id'] }}" data-plan="{{ $plan['id'] }}" 
-                                                data-min="{{ $plan['minimum_amount'] }}" data-max="{{ $plan['maximum_amount'] }}" class="btn btn-danger btn-rounded deposit-btn">Deposit</a>
+                                                data-min="{{ $plan['minimum_amount'] }}" data-max="{{ $plan['maximum_amount'] }}" class="btn btn-danger btn-rounded deposit-btn">Invest</a>
                                             </div>
                                         </div>
                                     </div>
@@ -42,12 +64,13 @@
                             </div>
                         </div>
                         @endforeach
-                    </div>
-                </div>
-                @include('user.layouts.footer')
-            </div>
+                        </div>
+
+		            </section>
+		<!-- /.content -->
+	            </div>
         </div>
-        <div class="modal fade" id="deposit-modal">
+         <div class="modal fade" id="deposit-modal">
             <h4 class="modal-title text-dark">Deposit To Plan</h4>
             <div class="modal-body">
                 <form class="page-form deposit-form" action="">
@@ -89,7 +112,7 @@
                 </form>
             </div>
         </div>
-
+    
         <div class="modal fade" id="success-modal">
             <h4 class="modal-title text-uppercase text-dark"><b class="text-uppercase deposit-wallet"></b> DEPOSIT</h4>
             <div class="modal-body font-weight-bold">
@@ -129,13 +152,15 @@
                 </div>
             </div>
         </div>
+
+
+        @include('user.layouts.footer')
         @include('user.layouts.general-scripts')
         <script src="{{ asset('assets/js/lib/sweetalert/sweetalert.min.js') }}"></script>
-        <script src="{{ asset('assets/plugins/qrcode/qrcode.min.js') }}"></script>
-        <script src="{{ asset('assets/plugins/clipboard/clipboard.min.js') }}"></script>
-        <script src="{{  asset('assets/js/custom.min.js') }}"></script>
+        <script src="{{ asset('assets/js/lib/qrcode/qrcode.min.js') }}"></script>
+        <script src="{{ asset('assets/js/lib/clipboard/clipboard.min.js') }}"></script>
         <script src="{{  asset('assets/js/fn.js') }}"></script>
         <script src="{{  asset('assets/js/main.js') }}"></script>
         <script src="{{  asset('assets/js/user.deposit.js') }}"></script>
     </body>
-    </html>
+</html>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,6 +4,23 @@
             @include('user.layouts.sidebar')
             <div class="content-wrapper">
                 <div class="container-full">
+                     <div class="content-header">
+                        <div class="d-flex align-items-center">
+                            <div class="me-auto">
+                                <h4 class="page-title">Home</h4>
+                                <div class="d-inline-block align-items-center">
+                                    <nav>
+                                        <ol class="breadcrumb">
+                                            <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-home-outline"></i></a></li>
+                                            <li class="breadcrumb-item" aria-current="page">User</li>
+                                            <li class="breadcrumb-item active" aria-current="page">Home</li>
+                                        </ol>
+                                    </nav>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
                     <!-- Main content -->
                     <section class="content">			
                         
