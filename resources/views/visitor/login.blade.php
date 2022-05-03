720f3d551dee6f741519ecf8da6ed71ee710719d
Darlington <fritzdultimate@gmail.com>
2022-05-03 19:40:19 +0100

    register and login - not yet modified

--- resources/views/visitor/login.blade.php
+++ resources/views/visitor/login.blade.php
@@ -1,48 +1,214 @@
-        @include('visitor.layouts.header')
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12">
-                    <div class="login_top_box d-flex justify-content-center align-items-center">
-                        @include('user.layouts.errors')
-                        <div class="login_form_wrapper">
-                            <div class="sv_heading_wraper heading_wrapper_dark dark_heading hwd">
+<!doctype html>
+<html class="no-js" lang="">
 
-                                <h3> login </h3>
 
-                            </div>
-                            <p>Enter Your Login details</p>
-                            <form class="page-form login-form">
-                                <div class="form-group icon_form comments_form">
 
-                                    <input required type="text" class="form-control require" name="login" placeholder="Username Or Email">
-                                
-                                </div>
-                                <div class="form-group icon_form comments_form">
+<!-- Mirrored from skylifeinvestmentfunds.com/secured/auth/account-login.php by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 24 Apr 2022 21:45:34 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Skylife Investment Funds | Login to your account</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.html">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="css/fontawesome-all.min.css">
+    <!-- Flaticon CSS -->
+    <link rel="stylesheet" href="font/flaticon.css">
+    <!-- Google Web Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;display=swap" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="style.css">
 
-                                    <input required name="password" type="password" class="form-control require" placeholder="Password *">
-                                
-                                </div>
+    <!-- jquery-->
+    <script src="js/jquery-3.5.0.min.js"></script>
+    <link rel="stylesheet" href="../sweetalert2/dist/sweetalert2.min.css">
+</head>
+    <style>
+    .kiksksk992mmw {
+        background: #01123c !important;
+        color: #fff !important;
+    }
+    </style>
+
+
+<body>
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+    <video
+        style="position: absolute; z-index: 0; width: 100%; height: 100vh; top: 0px; left: 0px; overflow: hidden; opacity: 1;         user-select: none; display:block !important; object-fit: cover !important"
+        autoplay muted loop playsinlines src="../../site-images/bg-chc.mp4"
+        poster="h4-slider-1-background-img.jpg"></video>
+
+    <section class="fxt-template-animation fxt-template-layout24" style="position: relative;">
+        <!-- data-bg-image="site-images/h4-slider-1-background-img.jpg" -->
+
+
+
+
+
+        <div class="overlay"
+            style="position: absolute; z-index: 0; width: 100%; height: 100%; top: 0px; left: 0px; overflow: hidden; opacity: 1; background: #00000063;">
+        </div>
+
+        <!-- linear-gradient(45deg, #528BC9 0%, #54ceff 100%);  -->
+
+        <!-- Video Area Start Here -->
+        <div class="container" style="width: auto; max-width: 93% !important">
+
+            <div class="row align-items-center justify-content-center"
+                style="background: linear-gradient(45deg, #012c6ae6 100%, #012c6ae6 100%); padding-top: 30px; padding-bottom: 30px">
+                <div class="col-lg-3">
+                    <div class="fxt-header">
+                        <a href="#" class="fxt-logo"><img style="height: 100px;
+    object-fit: contain;" src="../../site-images/site_logo/bg-rmv.png" alt="Logo"></a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="fxt-content">
+                        <h2 style="font-size: 27px; margin-bottom: 25px; text-transform: uppercase">Log into your
+                            account</h2>
                         
-                                <div>
-                                    <button class="btn btn-warning w-100 input-rounded" type="submit">
-                                        <span class="form-loading d-none px-5">
-                                            <i class="fa fa-sync fa-spin"></i>
-                                        </span>
-                                        <span class='submit-text'>
-                                            Login
-                                        </span>
-                                    </button>
+
+                        
+
+                        <div class="fxt-form">
+                            <form method="POST" action="#">
+                                <div class="form-group">
+                                    <div class="fxt-transformY-50 fxt-transition-delay-1">
+                                        <input type="text" id="general_username" class="form-control" name="username"
+                                            placeholder="Username" required="required">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="fxt-transformY-50 fxt-transition-delay-2">
+                                        <input id="general_password" type="password" class="form-control" name="password"
+                                            placeholder="Password" required="required">
+                                        <i toggle="#password" class="fa fa-fw fa-eye toggle-password field-icon"></i>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="fxt-transformY-50 fxt-transition-delay-3">
+                                        <div class="fxt-checkbox-area">
+                                            <div class="checkbox">
+                                                <input id="checkbox1" type="checkbox">
+                                                <label for="checkbox1">Keep me logged in</label>
+                                            </div>
+                                            <a href="reset-password.html" class="switcher-text">Forgot Password</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="fxt-transformY-50 fxt-transition-delay-4">
+                                        <button type="submit" name="login" id="general_login" class="fxt-btn-fill kiksksk992mmw">Login <span id="loaderIcon5"></span></button>
+                                    </div>
                                 </div>
                             </form>
-                            <div class="dont_have_account float_left">
-                                <p>Don’t have an acount ? <a href="register.html">Sign up</a></p>
+                        </div>
+                        <div class="fxt-footer">
+                            <div class="fxt-transformY-50 fxt-transition-delay-9">
+                                <p>Don't have an account?<a class="" href="create-new-account.html"
+                                        class="switcher-text2">Register</a>
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        @include('visitor.layouts.footer')
+    </section>
+
+    <script type="text/javascript" id="zsiqchat">var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode: "3e40217ff06e26638dff4410e5ba8e241f66b3caeffaee75d3d0d194726b4eda38d4acc225e44ff5009462ded0261ab4", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zoho.eu/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);</script>
+
+
+<!-- Popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Imagesloaded js -->
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <!-- Validator js -->
+    <script src="js/validator.min.js"></script>
+    <!-- Custom Js -->
+    <script src="js/main.js"></script>
+    
+       
+    <script src="js/jquery.mb.YTPlayer.min.js"></script>
+    <!-- Validator js -->
+
+    <script src="js/ui-alerts.min.js"></script>
+    <script src="../sweetalert2/dist/sweetalert2.min.js"></script>
+
+    <script>
+    $(document).ready(function(){
+
+    
+         // for ajax login
+
+                                            $("#general_login").on('click', function(event){
+                                                    event.preventDefault();
+                                                var general_username = $('#general_username').val();
+                                                var general_password = $('#general_password').val();
+                                                var url = "../inc/process_login_ajax.html";
+          
+                                                if(!general_username || !general_password ){           
+                                                    Swal.fire(
+                                                        'Oh Sorry',
+                                                        'Fields cannot be blank',
+                                                        'error'
+                                                    )
+                                                }else{
+                                                   $.ajax({
+                                                   type: "POST",
+                                                   url : url,
+                                                   data: {username : general_username, password : general_password },
+                                                   success: function(data, request, settings){
+                                                   // alert(data);
+
+                                                   if( data == "log_changed"){
+                                                      $('#general_login').prop('disabled', true);
+                                                      window.location="det_security.html";
+                                                   }else if( data == "log_true"){
+                                                      $('#general_login').prop('disabled', true);
+                                                      window.location="../../index-2.html";
+                                                      // alert(data);
+                                                   }else{
+                                                    Swal.fire(
+                                                       'Oh Sorry',
+                                                       data,
+                                                       'error'
+                                                    )
+                                                   }
+
+                                                   $('#loaderIcon5').hide();
+                                                   $('#general_username').val("");
+                                                   $('#general_password').val("");
+                                                              
+                                                },
+                                                beforeSend: function(data, request, settings){
+                                                  $('#loaderIcon5').html('<img src="../ajaxloader/ajax-loader3.gif"/>');
+                                                },
+  
+                                                });
+            
+                                            }
         
-    </body>
+                                            });
+                                            // end of ajax login
+    });
+</script>
+
+</body>
+
+
+
+<!--   -->
+
+<!-- Mirrored from skylifeinvestmentfunds.com/secured/auth/account-login.php by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 24 Apr 2022 21:45:55 GMT -->
 </html>
